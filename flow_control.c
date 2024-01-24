00380129c2abff966e444faad9240f7810b24a2c
abhinavchadaga <abhinav.chadaga@utexas.edu>
2024-01-24 17:30:44 -0600

    add switch statement

--- flow_control.c
+++ flow_control.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
 
 int main(void) {
-  // if/else statement
+  // if/else statement demo
   int x = 10;
   if (x > 5) {
     printf("x is greater than 5!\n");
   } else {
     printf("x is less than 5\n");
   }
+  printf("\n");
 
-  // for loop
+  // for loop demo
   printf("For loop:\n");
   for (int i = 0; i < 10; i++) {
     printf("Loop iteration: %d\n", i);
   }
+  printf("\n");
 
   // while loop (equivalent way as for loop)
   printf("While loop:\n");
@@ -21,6 +23,24 @@ int main(void) {
   while (i < 10) {
     printf("Loop iteration: %d\n", i);
     i++;
+  }
+  printf("\n");
+
+  // switch statement demo
+  int num = 3;
+  switch (num) {
+    case 1:
+      printf("You entered one.\n");
+      break;
+    case 2:
+      printf("You entered two.\n");
+      break;
+    case 3:
+      printf("You entered three.\n");
+      break;
+    default:
+      printf("You entered a number other than 1, 2, or 3.\n");
+      break;
   }
 
   return 0;
