bd2727be4056caa9421fc6785b3580f70473772d
DhruvNistala <dhruv.nistala@gmail.com>
2024-01-30 13:33:42 -0600

    fileio and some more info abt make

---
+++ FileIO/console.c
@@ -0,0 +1,18 @@
+// Written by Dhruv Nistala on 1/30/2024
+
+#include  <stdio.h>
+
+int main() {
+    char buffer[100];
+
+    // output text to console
+    printf("Hello world!\n");
+
+    // input text from console
+    printf("enter a string: ");
+    fgets(buffer, sizeof(buffer), stdin);
+
+    printf("you entered %s", buffer);
+
+
+}
