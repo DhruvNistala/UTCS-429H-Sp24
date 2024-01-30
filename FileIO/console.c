b492cb0e2ec44c79520e452f1fae999d04ae9161
abhinavchadaga <abhinav.chadaga@utexas.edu>
2024-01-30 14:12:56 -0600

    format FileIO files

--- FileIO/console.c
+++ FileIO/console.c
@@ -1,16 +1,16 @@
 // Written by Dhruv Nistala on 1/30/2024
 
-#include  <stdio.h>
+#include <stdio.h>
 
 int main() {
-    char buffer[100];
+  char buffer[100];
 
-    // output text to console
-    printf("Hello world!\n");
+  // output text to console
+  printf("Hello world!\n");
 
-    // input text from console
-    printf("enter a string: ");
-    fgets(buffer, sizeof(buffer), stdin);
+  // input text from console
+  printf("enter a string: ");
+  fgets(buffer, sizeof(buffer), stdin);
 
-    printf("you entered %s", buffer);
+  printf("you entered %s", buffer);
 }
